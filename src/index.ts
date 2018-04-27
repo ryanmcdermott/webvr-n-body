936dbfd67f97454bae48abef4901e4697f7cd312
Ryan McDermott <ryantmcdermott@gmail.com>
2018-04-26 20:11:39 -0700

    Add test sphere

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import 'aframe';
 
-console.warn('success');
-console.warn('second success');
+const el = document.createElement('a-sphere');
+el.setAttribute('color', 'yellow');
+el.setAttribute('radius', '1');
+el.setAttribute('position', '0 0 -200');
+
+const scene = document.getElementById('galaxy-scene');
+scene.appendChild(el);
